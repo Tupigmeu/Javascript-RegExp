dce7a1c0f55901b26bb5c380befb2c9e7395c16b
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-01-17 17:02:09 -0300

    Update aula6.js

--- aula6.js
+++ aula6.js
@@ -1,10 +1,9 @@
-const {cpfs, cpfs2, ips} = require('./base');
+const {cpfs, ips} = require('./base');
 console.log(cpfs)
 
 const cpfValido = cpfs.match(/.[0-9]{3}?.[0-9]{3}?.[0-9]{3}?-.{2}/g)
 console.log(`Os cps válidos são:`)
 console.log(cpfValido)
-
 
 // 250 - 255 25[0-5]
 // 200 - 249 2[0-4][\d]
@@ -13,6 +12,7 @@ console.log(cpfValido)
 // 0 - 9 [\d]
 
 const ipValido = ips.match(/((25[0-5]|2[0-4][\d]|1[0-9]{2}|[1-9][\d]|[\d])\.){3}(25[0-5]|2[0-4][\d]|1[0-9]{2}|[1-9][\d]|[\d])/g)
+console.log(ips)
 console.log("Os ips válidos são")
 console.log(ipValido)
 
